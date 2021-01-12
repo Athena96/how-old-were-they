114a5abdcd456c761ea986aaecb543e91464d203
Jared Franzone <jaredfranzone@gmail.com>
2021-01-11 21:12:08 -0800

    show all movies then select for actors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { List, ListItem, ListItemContent, CardActions, CardTitle, CardText, Card } from 'react-mdl';
+import { Button, CardActions, CardTitle, CardText, Card } from 'react-mdl';
 import Spinner from 'react-bootstrap/Spinner'
 import '../App.css';
 
@@ -7,28 +7,28 @@ class Home extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { isLoading: false, movie: '', movieActors: [] };
+    this.state = { isLoading: false, videoResults: [], videoResultsMap: {}, videoSearch: '', videoActorResults: [] };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
   }
   handleChange(event) {
-    this.setState({ movie: event.target.value });
+    this.setState({ videoSearch: event.target.value });
   }
 
   handleSubmit(event) {
-    if (this.state.movie === "") {
+    if (this.state.videoSearch === '') {
       return
     }
     this.setState({ isLoading: true });
     let currentComponent = this;
-
     event.preventDefault();
 
     var unirest = require("unirest");
     var req = unirest("GET", "https://imdb8.p.rapidapi.com/title/find");
     req.query({
-      "q": this.state.movie
+      "q": this.state.videoSearch
     });
     console.log("API_KEY: " + process.env.API_KEY);
 
@@ -40,59 +40,81 @@ class Home extends Component {
 
     req.end(function (res) {
       if (res.error) throw new Error(res.error);
-      console.log(JSON.stringify(res.body.results[0]));
-      currentComponent.setState({ movieData: res.body.results[0] });
+      var queryResults = res.body.results.filter(function(video) {
+        if (video.titleType) {
+          return video.titleType.includes('tv') || video.titleType.includes('movie');
+        }
+      });
 
-      const movieYear = parseInt(res.body.results[0].year);
-      var movieActors = [];
-      for (const actor of res.body.results[0].principals) {
-        const id = actor.id.split("/")[2];
-        var nreq = unirest("GET", "https://imdb8.p.rapidapi.com/actors/get-bio");
-        nreq.query({
-          "nconst": id
-        });
-
-        nreq.headers({
-          "x-rapidapi-key": "e7d1372ca8msh640de66bbef1f29p158dc7jsn87b53fa8b82a",
-          "x-rapidapi-host": "imdb8.p.rapidapi.com",
-          "useQueryString": true
-        });
-
-        nreq.end(function (res) {
-          if (res.error) throw new Error(res.error);
-          const yr = parseInt(res.body.birthDate.split("-")[0])
-          const yob = res.body.birthDate.split("-")[0]
-          const age = movieYear - yr;
-          movieActors.push({ actor: actor.name, id: id, yob: yob, age: age, imageurl: res.body.image.url});
-          currentComponent.setState({ movieActors: movieActors, isLoading: false });
-        });
+      currentComponent.setState({videoResultsMap: {} });
+      var videoResMp = {}
+      for (var ob of queryResults) {
+        videoResMp[ob.id] = ob;
       }
+      currentComponent.setState({videoResultsMap: videoResMp });
+
+      currentComponent.setState({ videoResults: queryResults, isLoading: false });
+
     });
   }
 
-  renderMovie() {
-    if (this.state.movieData) {
-      const imgUrl = this.state.movieData.image.url;
-      const width = this.state.movieData.image.width/3 + "px";
-      const height = this.state.movieData.image.height/3 + "px";
+  handleSelect(e) {
+    let currentComponent = this;
+    this.setState({videoActorResults: this.state.videoResultsMap[e.target.id].principals,
+    videoResults: [this.state.videoResultsMap[e.target.id]] }); 
 
-      console.log(width)
-      console.log(height)
+    const movieYear = parseInt(this.state.videoResultsMap[e.target.id].year);
+    var movieActors = [];
+    for (const actor of this.state.videoResultsMap[e.target.id].principals) {
+      const id = actor.id.split("/")[2];
+      var unirest = require("unirest");
+      var nreq = unirest("GET", "https://imdb8.p.rapidapi.com/actors/get-bio");
+      nreq.query({
+        "nconst": id
+      });
 
-    return (
-      <div>
+      nreq.headers({
+        "x-rapidapi-key": "e7d1372ca8msh640de66bbef1f29p158dc7jsn87b53fa8b82a",
+        "x-rapidapi-host": "imdb8.p.rapidapi.com",
+        "useQueryString": true
+      });
 
-      <Card shadow={0} style={{height: "500px"}} >
-    <CardTitle expand style={{color: '#fff', background: 'url('+imgUrl+') center / cover'}}><h2><b>"{this.state.movieData.title}"</b></h2></CardTitle>
-    <CardText>
-     <h3>{this.state.movieData.year}</h3>
-    </CardText>
-</Card>
-      </div>
+      nreq.end(function (res) {
+        if (res.error) throw new Error(res.error);
+        if (res && res.hasOwnProperty('body') && res.body.hasOwnProperty('birthDate')) {
+          const yr = parseInt(res.body.birthDate.split("-")[0])
+          const yob = res.body.birthDate.split("-")[0]
+          const age = movieYear - yr;
+          movieActors.push({ actor: actor.name, id: id, yob: yob, age: age, imageurl: res.body.image.url });
+          currentComponent.setState({ videoActorResults: movieActors, isLoading: false });
+        }
 
-      
+      });
+    }
 
-    );
+  }
+
+  renderVideoResults() {
+    if (this.state.videoResults.length !== 0 ) {
+      var r = [];
+      for (const video of this.state.videoResults) {
+        if (video && video.hasOwnProperty('image')) {
+          const imgUrl = video.image.url;
+          r.push(
+              <Card shadow={0} style={{ margin: '10px', height: '550px'}} >
+                <CardTitle expand style={{ color: '#fff', background: 'url(' + imgUrl + ') center / cover' }}></CardTitle>
+                <CardText>
+
+                <Button id={video.id} onClick={this.handleSelect} style={{color: 'blue'}}>"{video.title}" | {video.year}</Button>
+
+                </CardText>
+              </Card>
+          );
+        }
+      }
+
+      return r;
+
     } else {
       return <></>
     }
@@ -100,23 +122,20 @@ class Home extends Component {
 
   renderActors() {
     var el = []
-    for (const actor of this.state.movieActors) {
+    for (const actor of this.state.videoActorResults) {
       const d = "Year Of Birth: " + actor.yob + " | Age in movie: " + actor.age;
       if (actor.age === 0) continue;
-      const a = 'url('+actor.imageurl+') center / cover'
       el.push(
-        <Card shadow={0} style={{margin: '20px', width: '256px', height: '256px', background: 'url('+actor.imageurl+') center / cover'}}>
-    <CardTitle expand />
-    <CardActions style={{height: '52px', padding: '5px', background: 'rgba(0,0,0,0.7)'}}>
-        <span style={{color: '#fff', fontSize: '14px', fontWeight: '500'}}>
-          {actor.actor}<br/>
-            {d}
-        </span>
-    </CardActions>
-</Card>
-        
-        
-        )
+        <Card shadow={0} style={{ margin: '10px', width: '256px', height: '256px', background: 'url(' + actor.imageurl + ') center / cover' }}>
+          <CardTitle expand />
+          <CardActions style={{ height: '52px', padding: '5px', background: 'rgba(0,0,0,0.7)' }}>
+            <span style={{ color: '#fff', fontSize: '14px', fontWeight: '500' }}>
+              {actor.actor}<br />
+              {d}
+            </span>
+          </CardActions>
+        </Card>
+      )
     }
     return el
   }
@@ -132,8 +151,8 @@ class Home extends Component {
 
         <form onSubmit={this.handleSubmit}>
           <label>
-            <b>Movie Title</b>: <br />
-            <input type="text" value={this.state.movie} onChange={this.handleChange} />
+            <b>Movie or TV show Title</b>: <br />
+            <input type="text" value={this.state.videoSearch} onChange={this.handleChange} />
           </label>
           <input type="submit" value="Search" />
         </form>
@@ -144,15 +163,13 @@ class Home extends Component {
           </Spinner>
         </div> : <></>}
 
-        {this.renderMovie()}
- 
+        {this.renderVideoResults()}
+
         <hr />
 
-        <div>
-          <List style={{ width: '350px' }}>
-            {this.renderActors()}
-          </List>
-        </div>
+        {this.state.videoActorResults.length !== 0 ? 
+          this.renderActors()
+         : <></>}
 
         <footer align="center" style={{ background: "clear" }}>
           <small >© Copyright | All rights reserved </small>
